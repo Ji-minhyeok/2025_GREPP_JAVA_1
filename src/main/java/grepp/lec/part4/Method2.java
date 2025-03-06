cbc0c8466a87528151cfd50f52e7da397fc88837
eddie1031 <archaco123@gmail.com>
2025-03-06 16:09:42 +0900

    6. 함수와 오버로딩

---
+++ src/main/java/grepp/lec/part4/Method2.java
@@ -0,0 +1,13 @@
+package grepp.lec.part4;
+
+public class Method2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    static void printTest() {
+        System.out.println("Method2.printTest"); // soutm
+    }
+
+}
---
+++ src/main/java/grepp/lec/part4/inner/Method3.java
@@ -0,0 +1,23 @@
+package grepp.lec.part4.inner;
+
+import grepp.lec.part4.Method1;
+
+public class Method3 {
+    public static void main(String[] args) {
+
+//        Method2.printTest();
+        Method1.printTest(); // public
+
+        System.out.println();
+
+        Method1.printTest4();
+        /*
+        public
+        default(Package)
+        private
+        Method2.printTest
+         */
+
+    }
+
+}
